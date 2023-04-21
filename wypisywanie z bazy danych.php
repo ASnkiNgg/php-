096a7756d58342b70a33cc4b4bded02e4b531c7d
ASnkiNgg <105479648+ASnkiNgg@users.noreply.github.com>
2023-04-21 10:31:09 +0200

    Create wypisywanie z bazy danych.php

---
+++ wypisywanie z bazy danych.php
@@ -0,0 +1,11 @@
+if(isset($_POST["cos"])){
+    $tekst = $_POST["tekst"];
+    $wyswietl = "Select * from imie";
+    $zwrocenie = mysqli_query($connection,$wyswietl);
+    while($row=mysqli_fetch_row($zwrocenie)){
+        echo "$row[0]";
+        echo "";
+        echo "$row[1],<br>";
+    }
+
+}
